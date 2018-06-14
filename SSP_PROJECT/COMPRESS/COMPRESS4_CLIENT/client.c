2b5a3cf79e22a59a35a2c5c23bc6fb105d8ac3b9
tina0711 <primo711@naver.com>
2018-06-14 14:05:35 +0000

    Create client.c

---
+++ SSP_PROJECT/COMPRESS/COMPRESS4_CLIENT/client.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <pthread.h>
+#include "./client.h"
+
+char name[NAMESIZE] = "[Default]";
+int sock;
+
+int connectServer(char* clientName)
+{
+	 struct sockaddr_in serv_addr;
+	 pthread_t rcv_thread;
+	 void* thread_result;
+
+	 printf("connectServer\n");
+
+	 sock = socket(PF_INET, SOCK_STREAM, 0);
+	 if(sock == -1)
+	  error_handling("socket() error");
+
+	 memset(&serv_addr, 0, sizeof(serv_addr));
+	 serv_addr.sin_family = AF_INET;
+	 serv_addr.sin_addr.s_addr=inet_addr("127.0.0.1");
+	 serv_addr.sin_port=htons(PORT);
+
+	 if(connect(sock,(struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1)
+	  error_handling("connect() error!");
+
+	 return 0;
+}
+
+int send_message(char* message)
+{
+	printf("send message = %s\n", message);
+	return write(sock, message, strlen(message));
+}
+
+int recv_message(char* message)
+{
+	 char name_message[BUFSIZE];
+	 int str_len;
+	  str_len = read(sock, name_message, NAMESIZE+BUFSIZE-1);
+
+//	  printf("strlen[%d]\n",str_len);
+
+	  if(str_len <=0)
+	  {
+		  return (void*)1;
+	  }
+	  name_message[str_len]=0;
+	  memcpy(message, name_message, str_len);
+	  message[str_len] =0;
+
+	  return str_len;
+}
+
+void error_handling(char * message)
+{
+	 fputs(message ,stderr);
+	 fputc('\n', stderr);
+}
+
+
