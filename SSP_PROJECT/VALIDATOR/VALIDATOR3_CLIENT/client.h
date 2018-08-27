fd7a9ee3462c41d4116ce835db5bb6d697600548
tina0711 <primo711@naver.com>
2018-08-27 12:46:04 +0000

    Create client.h

---
+++ SSP_PROJECT/VALIDATOR/VALIDATOR3_CLIENT/client.h
@@ -0,0 +1,13 @@
+#ifndef CLIENT_H_
+#define CLIENT_H_
+
+#define BUFSIZE 1024
+#define NAMESIZE 100
+#define PORT 1111
+
+int send_message(char* message);
+int recv_message(char* message);
+int connectServer();
+void error_handling(char * message);
+
+#endif /* CLIENT_H_ */
