79c02e34fb69f45309da3276a2e029084de6715f
tina0711 <primo711@naver.com>
2018-08-27 12:36:36 +0000

    Update client.h

--- SSP_PROJECT/MERGE/MERGE4_CLIENT/client.h
+++ SSP_PROJECT/MERGE/MERGE4_CLIENT/client.h
@@ -1,9 +1,12 @@
 #ifndef CLIENT_H_
+#define CLIENT_H_
 
+#define BUFSIZE 1024
+#define NAMESIZE 100
+#define PORT 1111
 
-
-
-
-
-
+int send_message(char* message);
+int recv_message(char* message);
+int connectServer(char* clientName);
+void error_handling(char * message);
 
