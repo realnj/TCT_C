287af3e9811ee96acad374745371856e640a26ec
tina0711 <primo711@naver.com>
2018-08-27 12:48:04 +0000

    Update server.h

--- SSP_PROJECT/VALIDATOR/VALIDATOR3_SERVER/server.h
+++ SSP_PROJECT/VALIDATOR/VALIDATOR3_SERVER/server.h
@@ -1,15 +1,15 @@
 #ifndef SERVER_H_
-#endif /* SERVER_H_ */
+#define SERVER_H_
 
-#endif /* SERVER_H_ */
-#endif /* SERVER_H_ */
+#include <stdio.h>
+#include <pthread.h>
 
-#endif /* SERVER_H_ */
-#endif /* SERVER_H_ */
+#define BUFSIZE 1024
+#define PORT 1111
 
-#endif /* SERVER_H_ */
-#endif /* SERVER_H_ */
-#endif /* SERVER_H_ */
-#endif /* SERVER_H_ */
+int send_message(char * message);
+int recv_message(char * message);
+void error_handling(char * message);
+void connectServer();
 
 #endif /* SERVER_H_ */
